efff1dc4c939a5606fc6877018309894ce6c0fb7
Przemysław Kurowski <przemyslawmkurowski@gmail.com>
2026-05-17 22:20:33 +0200

    Naiwne rozwiązanie - BEZ sortowania

---
+++ Source/BRC/Private/ChallengeFunctionLibrary.cpp
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChallengeFunctionLibrary.h"
+
+void UChallengeFunctionLibrary::BRC_Naive(const FString& InInputName)
+{
+	// open file
+	const FString Path = FPaths::ProjectContentDir() + TEXT("Data/") + InInputName;
+	TUniquePtr<FArchive> File(IFileManager::Get().CreateFileReader(*Path));
+	
+	// Prepare RET
+	TMap<FString, FBRCStruct_Naive> Data;
+	
+	int soonToBeEnum = 0;
+	// 100 bytes, plus termination symbol
+	constexpr int bufferSize = 101;
+	ANSICHAR buffer[bufferSize];
+	// buffer is null-terminated, so making last character null should work just fine
+	buffer[0] = 0;
+	int bufferIndex = 0;
+	
+	FString Station;
+	
+	while(!File->AtEnd())
+	{
+		ANSICHAR byte;
+		
+		// this line stream from File to byte. Do not ask.
+		*File << byte;
+		
+		if (soonToBeEnum == 0 && byte == ';')
+		{
+			buffer[bufferIndex] = 0;
+			Station = UTF8_TO_TCHAR(buffer);
+			buffer[0] = 0;
+			bufferIndex = 0;
+			soonToBeEnum = 1;
+		}
+		else if (soonToBeEnum == 1 && byte == '\n')
+		{
+			buffer[bufferIndex] = 0;
+			double candidate = FCStringAnsi::Atof(buffer);
+			buffer[0] = 0;
+			bufferIndex = 0;
+			soonToBeEnum = 0;
+			
+			//here add value to TMap
+			if (FBRCStruct_Naive* row = Data.Find(Station))
+			{
+				row->min = row->min < candidate ? row->min : candidate;
+				row->max = row->max > candidate ? row->max : candidate;
+				row->avg = (row->avg * row->count + candidate) / (row->count + 1);
+				row->count = row->count + 1; 
+			}
+			else
+			{
+				Data.Add(Station, FBRCStruct_Naive(candidate));
+			}
+			
+		}
+		else
+		{
+			buffer[bufferIndex] = byte;
+			++bufferIndex;
+		}
+	}
+	
+	for(auto row : Data)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s;%.1f;%.1f;%.1f"), *row.Key, row.Value.min, row.Value.avg, row.Value.max);
+	}
+	
+	File->Close();
+}
---
+++ Source/BRC/Public/ChallengeFunctionLibrary.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ChallengeFunctionLibrary.generated.h"
+
+struct FBRCStruct_Naive
+{
+	FBRCStruct_Naive() : min(-100), max(100), avg(0), count(0) {}
+	FBRCStruct_Naive(double value) : min(value), max(value), avg(value), count(1) {}
+	
+	// specification limit range to <-99.9, 99.9>
+	double min;
+	double max;
+	double avg;
+	double count;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class BRC_API UChallengeFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	static void BRC_Naive(const FString& InInputName);
+	
+};
